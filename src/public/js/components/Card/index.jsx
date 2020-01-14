54f74ad8329b73ea78a25fa028a37f2121fffe72
Daniel <olgierd.dan@gmail.com>
2020-01-14 12:53:45 +0100

    Add 'play' button to card

--- src/public/js/components/Card/index.jsx
+++ src/public/js/components/Card/index.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 
 export default function Card({ expressions }) {
   const { engWord, polWord, engSen, polSen } = expressions;
 
   return (
     <div className="card">
-      <div className="card__words">
-        <span className="card__eng-word">{engWord}</span>
-        <ChevronRightIcon style={{ color: "#999" }} />
-        <span className="card__pol-word">{polWord}</span>
+      <div className="card__left-side">
+        <div className="card__words">
+          <span className="card__eng-word">{engWord}</span>
+          <ChevronRightIcon style={{ color: "#999" }} />
+          <span className="card__pol-word">{polWord}</span>
+        </div>
+        <div className="card__sentences">
+          <p className="card__eng-sentence">{engSen}</p>
+          <p className="card__pol-sentence">{polSen}</p>
+        </div>
       </div>
-      <div className="card__sentences">
-        <p className="card__eng-sentence">{engSen}</p>
-        <p className="card__pol-sentence">{polSen}</p>
+      <div className="card__right-side">
+        <PlayCircleOutlineIcon style={{ fontSize: 40, color: "#999" }} />
       </div>
     </div>
   );
