fb89c84116027965ec63b34ef6803a492bb465ce
Daniel <olgierd.dan@gmail.com>
2020-01-17 20:12:53 +0100

    Add global audio controls component

---
+++ src/public/js/components/AudioControls/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import PauseIcon from "@material-ui/icons/Pause";
+import StopIcon from "@material-ui/icons/Stop";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+
+export default function AudioControls({
+  onSkipPrevious,
+  onSkipNext,
+  onPause,
+  onResume,
+  onStop,
+  paused
+}) {
+  return (
+    <div className="audio-controls">
+      <SkipPreviousIcon
+        onClick={onSkipPrevious}
+        className="audio-controls__control audio-controls__control--skip"
+      />
+      {paused ? (
+        <PlayArrowIcon onClick={onResume} className="audio-controls__control" />
+      ) : (
+        <PauseIcon onClick={onPause} className="audio-controls__control" />
+      )}
+      <StopIcon onClick={onStop} className="audio-controls__control" />
+      <SkipNextIcon
+        onClick={onSkipNext}
+        className="audio-controls__control audio-controls__control--skip"
+      />
+    </div>
+  );
+}
--- src/public/js/containers/Navigation/ToolbarManager/index.jsx
+++ src/public/js/containers/Navigation/ToolbarManager/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Toolbar from "../../../components/Toolbar";
+import AudioControls from "../../../components/AudioControls";
 
 export default class ToolbarManager extends Component {
   render() {
     return (
       <div className="toolbar-bottom">
-        <Toolbar />
+        {/* <Toolbar /> */}
+        <AudioControls />
       </div>
     );
   }
