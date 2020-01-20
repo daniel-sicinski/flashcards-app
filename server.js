fdc97be78998dde9db40d063eda242fcfc1226f9
Daniel <olgierd.dan@gmail.com>
2020-01-20 16:42:47 +0100

    Connect app to mongoDB

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const app = require("./src/app");
+const mongoose = require("mongoose");
+
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = require("dotenv");
+  dotenv.config({ path: "./.env" });
+}
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to mongoose"));
 
 app.listen(process.env.PORT || 3000);
