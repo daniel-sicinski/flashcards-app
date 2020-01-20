4cae51a76fff279f0ad16e573d10f794789a8de7
Daniel <olgierd.dan@gmail.com>
2020-01-20 10:57:36 +0100

    Add routing for playlists

--- src/public/js/App.jsx
+++ src/public/js/App.jsx
@@ -4,16 +4,28 @@ import Cards from "./containers/Cards/Cards";
 import AudioManager from "./containers/AudioManager";
 import { fetchCardsStart } from "./store/actions/cardsActions";
 import Navigation from "./containers/Navigation";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
   componentDidMount() {
     this.props.fetchCardsStart();
   }
 
+  Playlists = () => <h1>Playlists!</h1>;
+  PlaylistView = () => <h1>Playlist view!</h1>;
+
   render() {
     return (
       <>
-        <Cards />
+        <Switch>
+          <Route path="/" exact component={Cards} />
+          <Route
+            path="/playlists/:playlistId"
+            exact
+            component={this.PlaylistView}
+          />
+          <Route path="/playlists" exact component={this.Playlists} />
+        </Switch>
         <Navigation />
         <AudioManager />
       </>
--- src/public/js/index.jsx
+++ src/public/js/index.jsx
@@ -3,10 +3,13 @@ import ReactDom from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
