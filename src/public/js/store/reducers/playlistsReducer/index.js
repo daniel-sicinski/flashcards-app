1b8cb32210a938da33cabc0fac1a14207f3e1c25
Daniel <olgierd.dan@gmail.com>
2020-01-21 17:29:38 +0100

    Add reducer for playlists

---
+++ src/public/js/store/reducers/playlistsReducer/index.js
@@ -0,0 +1,76 @@
+import {
+  FETCH_PLAYLISTS_START,
+  FETCH_PLAYLISTS_ERROR,
+  FETCH_PLAYLISTS_SUCCESS,
+  FETCH_PLAYLIST_START,
+  FETCH_PLAYLIST_SUCCESS,
+  FETCH_PLAYLIST_ERROR,
+  ADD_PLAYLIST_START,
+  ADD_PLAYLIST_SUCCESS,
+  ADD_PLAYLIST_ERROR,
+  DELETE_PLAYLIST_START,
+  DELETE_PLAYLIST_SUCCESS,
+  DELETE_PLAYLIST_ERROR,
+  UPDATE_PLAYLIST_START,
+  UPDATE_PLAYLIST_SUCCESS,
+  UPDATE_PLAYLIST_ERROR
+} from "../../actions/playlistsActions/actionNames";
+
+const initialState = {
+  playlistsData: {},
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_PLAYLISTS_START:
+    case FETCH_PLAYLIST_START:
+    case ADD_PLAYLIST_START:
+    case UPDATE_PLAYLIST_START:
+    case DELETE_PLAYLIST_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case FETCH_PLAYLISTS_SUCCESS:
+      return {
+        ...state,
+        playlistsData: action.payload.playlistsData,
+        loading: false
+      };
+    case ADD_PLAYLIST_SUCCESS:
+    case FETCH_PLAYLIST_SUCCESS:
+    case UPDATE_PLAYLIST_SUCCESS:
+      const playlistId = action.payload.playlist._id;
+      return {
+        ...state,
+        playlistsData: {
+          ...state.playlistsData,
+          [playlistId]: action.payload.playlist
+        },
+        loading: false
+      };
+    case DELETE_PLAYLIST_SUCCESS:
+      const playlistsDataCopy = { ...state.playlistsData };
+      delete playlistsDataCopy[action.payload.playlistId];
+
+      return {
+        ...state,
+        playlistsData: playlistsDataCopy,
+        loading: false
+      };
+    case UPDATE_PLAYLIST_ERROR:
+    case DELETE_PLAYLIST_ERROR:
+    case FETCH_PLAYLISTS_ERROR:
+    case FETCH_PLAYLIST_ERROR:
+    case ADD_PLAYLIST_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error
+      };
+    default:
+      return state;
+  }
+};
--- src/public/js/store/reducers/rootReducer.js
+++ src/public/js/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import audioReducer from "./audioReducer";
 import cardsReducer from "./cardsReducer";
+import playlistsReducer from "./playlistsReducer";
 
 export default combineReducers({
   audio: audioReducer,
-  cards: cardsReducer
+  cards: cardsReducer,
+  playlists: playlistsReducer
 });
