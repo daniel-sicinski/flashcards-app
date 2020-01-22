e0bbfb1d3ef98fc996055bc61086add9412abd49
Daniel <olgierd.dan@gmail.com>
2020-01-22 20:26:23 +0100

    Add playlist nav component

---
+++ src/public/js/components/PlaylistsNav/PlaylistsNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PlaylistsNav() {
+  return (
+    <>
+      <div className="playlist-nav__option">Dodaj playlistę</div>
+      <div className="playlist-nav__option">Edytuj playlistę</div>
+      <div className="playlist-nav__option">Usuń playlistę</div>
+    </>
+  );
+}
