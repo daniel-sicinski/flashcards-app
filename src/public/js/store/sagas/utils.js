738f7d0636c002a10f870c2925cd29405ef3d4dc
Daniel <olgierd.dan@gmail.com>
2020-02-03 10:58:00 +0100

    mapArrayToObject now throws errors if wrong params provided

--- src/public/js/store/sagas/utils.js
+++ src/public/js/store/sagas/utils.js
@@ -1,4 +1,16 @@
 export const mapArrayToObjectByIds = (array, idProp) => {
+  if (array.length === 0) return;
+
+  if (!idProp) {
+    throw new Error("idProp should be provided");
+  }
+
+  const wrongIdProp = array[0][idProp] === undefined;
+
+  if (wrongIdProp) {
+    throw new Error("wrong idProp provided");
+  }
+
   return array.reduce((result, item) => {
     result[item[idProp]] = item;
     return result;
