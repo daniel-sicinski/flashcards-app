72a30271be2176b2c5e6ee6b522e8bff171eeadb
Daniel <olgierd.dan@gmail.com>
2020-02-06 20:22:57 +0100

    Add login component

--- src/public/js/App.jsx
+++ src/public/js/App.jsx
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import AllCardsView from "./components/AllCardsView/AllCardsViewContainer";
-import AudioManager from "./components/AudioManager/AudioManager";
-import { fetchCardsStart } from "./store/actions/cardsActions";
-import Navigation from "./components/Navigation/NavigationContainer";
 import { Route, Switch } from "react-router-dom";
-import Playlists from "./components/Playlists/PlaylistsContainer";
-import PlaylistView from "./components/PlaylistView/PlaylistViewContainer";
-import PlaylistEditView from "./components/PlaylistEditView/PlaylistEditViewContainer";
-import NewPlaylistView from "./components/NewPlaylistView/NewPlaylistViewContainer";
 import { setDeviceType } from "./store/actions/UIActions";
 import { MIN_DESKTOP_WIDTH } from "./config";
 import Register from "./components/Register/RegisterContainer";
+import Login from "./components/Login/LoginContainer";
 import AppContainer from "./components/AppContainer/AppContainerContainer";
 
 class App extends Component {
   componentDidMount() {
-    // this.props.fetchCardsStart();
-
     this.handleDeviceType();
     window.addEventListener("resize", this.handleDeviceType);
   }
@@ -36,6 +27,7 @@ class App extends Component {
       <>
         <Switch>
           <Route path="/register" exact component={Register} />
+          <Route path="/login" exact component={Login} />
           <Route component={AppContainer} />
         </Switch>
       </>
