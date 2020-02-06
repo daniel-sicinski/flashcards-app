5c014efbe816ee39692c70b59a4c18c078f88e20
Daniel <olgierd.dan@gmail.com>
2020-02-06 17:01:13 +0100

    Remove redundant console.logs

--- src/lib/utils/userIsLoggedIn.js
+++ src/lib/utils/userIsLoggedIn.js
@@ -1,8 +1,6 @@
 const StatusError = require("./statusError");
 
 module.exports = (req, res, next) => {
-  console.log("Request session:");
-  console.log(req.session);
   if (req.session && req.session.userId) return next();
   return next(new StatusError("Unauthorized", 401));
 };
