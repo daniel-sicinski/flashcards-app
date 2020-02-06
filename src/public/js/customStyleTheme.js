b10f9bc0c0178422a6c60f41c8b9dc49dd2506e3
Daniel <olgierd.dan@gmail.com>
2020-02-06 16:55:31 +0100

    Add styling theme for material ui

---
+++ src/public/js/customStyleTheme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ffd581",
+      light: "#ffe0a1",
+      dark: "#d3b06a"
+    }
+  }
+});
