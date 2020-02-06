a4509013b15e07eaa90125f22d2a925aaa16acd4
Daniel <olgierd.dan@gmail.com>
2020-02-06 16:54:09 +0100

    Change button component

--- src/public/js/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button({ children, ...rest }) {
-  return (
-    <button className="button" {...rest}>
-      {children}
-    </button>
-  );
-}
