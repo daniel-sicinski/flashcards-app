0b798ce670aaacd69a98342f6d8ac9146fb6721f
Daniel <olgierd.dan@gmail.com>
2020-02-11 11:16:53 +0100

    Add service worker to cache app shell

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,37 @@
+const CACHE_STATIC_NAME = "cache-static-v1";
+const urlsToCache = [
+  "/",
+  "/bundle.js",
+  "/index.html",
+  "/main.css",
+  "/manifest.json",
+  "/assets/icons/144.png",
+  "https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700&display=swap"
+];
+
+const cacheAppShell = async () => {
+  const cache = await caches.open(CACHE_STATIC_NAME);
+  return cache.addAll(urlsToCache);
+};
+
+const handleRequest = async request => {
+  const pathname = new URL(request.url).pathname;
+
+  switch (pathname) {
+    case urlsToCache.includes(pathname) && pathname:
+      const cache = await caches.open(CACHE_STATIC_NAME);
+      return cache.match(request);
+    default:
+      return fetch(request);
+  }
+  // user routes:
+  // logout => may be need to do backgroundSync in that > remove userName from LS and on resumed connect > send logout request automatically
+};
+
 self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(cacheAppShell());
+
   console.log("SW installed!");
 });
 
@@ -7,5 +40,5 @@ self.addEventListener("activate", e => {
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(handleRequest(e.request));
 });
